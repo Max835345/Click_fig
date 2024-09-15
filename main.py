6a3dbfbd36c6460cec57d78bbeb92061202cd530
Max <79370059+Max835345@users.noreply.github.com>
2024-09-15 22:18:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import pygame
 from pygame.locals import *
 
-root = pygame.display.set_mode((640, 480))
+root = pygame.display.set_mode((700, 700))
 pygame.display.set_caption('CLICK_FIG')
+pygame.display.update()
+
+r_ps = None
+l_ps = None
 
 run = True
 while run:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             run = False
+
         if i.type == pygame.MOUSEBUTTONDOWN:
             if i.button == 1:
-                root.fill((0, 0, 0))
-                pygame.draw.circle(root, (225, 0, 50), i.pos, 20)
-                pygame.display.update()
-            elif i.button == 3:
-                root.fill((0, 0, 0))
-                pygame.draw.circle(root, (0, 0, 225), i.pos, 20)
-                pygame.draw.rect(root, (0, 225, 0), (i.pos[0] - 10, i.pos[1] - 10, 20, 20))
-                pygame.display.update()
+                r_ps = i.pos
+            if i.button == 3:
+                l_ps = i.pos
+
+        root.fill((0, 0, 0))
+        if r_ps is not None:
+            pygame.draw.circle(root, (225, 0, 50), r_ps, 20)
+            pygame.display.update()
+
+        if l_ps is not None:
+            pygame.draw.circle(root, (0, 0, 225), l_ps, 20)
+            pygame.draw.rect(root, (0, 225, 0), (l_ps[0] - 10, l_ps[1] - 10, 20, 20))
+            pygame.display.update()
 
 
 pygame.quit()
