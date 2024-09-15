4176e5e391971b9de9949c2f38577e4da3e8083f
Max <79370059+Max835345@users.noreply.github.com>
2024-09-15 20:25:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.locals import *
+
+root = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('CLICK_FIG')
+
+run = True
+while run:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            run = False
+        if i.type == pygame.MOUSEBUTTONDOWN:
+            if i.button == 1:
+                root.fill((0, 0, 0))
+                pygame.draw.circle(root, (225, 0, 50), i.pos, 20)
+                pygame.display.update()
+            elif i.button == 3:
+                root.fill((0, 0, 0))
+                pygame.draw.circle(root, (0, 0, 225), i.pos, 20)
+                pygame.draw.rect(root, (0, 225, 0), (i.pos[0] - 10, i.pos[1] - 10, 20, 20))
+                pygame.display.update()
+
+
+pygame.quit()
